bf0a83da3f35c3d03d320d43f9b0f1e046f9eb8b
Oscar Martinez <omartinez@pragma-tic.co>
2018-01-08 18:46:10 -0500

    Se ajustan las reuniones en los proyectos

--- pragmatic_helpdesk/models/helpdesk.py
+++ pragmatic_helpdesk/models/helpdesk.py
@@ -25,32 +25,67 @@ from datetime import datetime as dt
 import datetime
 import time
 from openerp.exceptions import ValidationError
-
+from openerp.osv import fields as fieldsOld, osv, orm
 
 _logger = logging.getLogger(__name__)
 
-class PragmaticProyecto(models.Model):
-    _inherit =  ['project.project']
+
     
-    use_meetings = fields.Boolean('Reuniones')
-    meeting_ids = fields.One2many('calendar.event','project_id', string='Reuniones')
-    meeting_needaction_count = fields.Integer(string='Need action')
-
-
-
+class PragmaticTarea(models.Model):
+    _inherit =  ['project.task']
+    meeting_id = fields.Many2one('calendar.event', string='Reunión')
 
 class PragmaticReunion(models.Model):
     _inherit =  ['calendar.event']
     
     project_id = fields.Many2one(string='Proyecto')
-    task_ids = fields.One2many('project.task','meeting_id','Compromisos')
+    task_ids = fields.One2many('project.task','meeting_id',string='Compromisos')
     goals = fields.Text(string='Objetivos')
-    development = fields.Text('Desarrollo')
-    description = fields.Text(string='Temas a tratar')
+    development = fields.Text(string='Desarrollo')
+    #description = fields.Text(string='Temas a tratar')
+
+
+class project(osv.Model):
+    _inherit = "project.project"
+
     
-class PragmaticTarea(models.Model):
-    _inherit =  ['project.task']
-    meeting_id = fields.Many2one('calendar.event', string='Reunión')
+    def _meeting_count(self, cr, uid, ids, field_name, arg, context=None):
+        Meeting = self.pool['calendar.event']
+        return {
+            project_id: Meeting.search_count(cr,uid, [('project_id', '=', project_id)], context=context)
+            for project_id in ids
+        }
+
+    def _meeting_needaction_count(self, cr, uid, ids, field_name, arg, context=None):
+        """Meeting = self.pool['calendar.event']
+        res = dict.fromkeys(ids, 0)
+        projects = Meeting.read_group(cr, uid, [('project_id', 'in', ids), ('message_needaction', '=', True)], ['project_id'], ['project_id'], context=context)
+        res.update({project['project_id'][0]: int(project['project_id_count']) for project in projects})
+        return res"""
+        return 0
+
+    _columns = {
+        'meeting_count': fieldsOld.function(_meeting_count, type='integer', string="Reuniones",),
+        'meeting_ids': fieldsOld.one2many('calendar.event', 'project_id', string="Reuniones",),
+        'meeting_needaction_count': fieldsOld.function(_meeting_needaction_count, type='integer', string="Reuniones",),
+        'use_meetings': fieldsOld.boolean("Usar reuniones"),
+        'label_meeting': fieldsOld.char('Usar reuniones como'),
+    }
+
+    _defaults = {
+        'use_meetings': False,
+        'label_meeting': 'Reuniones',
+    }
+
+    def on_change_use_tasks_or_issues(self, cr, uid, ids, use_tasks, use_issues, use_meetings, context=None):
+        values = {}
+        if use_tasks and not use_issues and not use_meetings:
+            values['alias_model'] = 'project.task'
+        elif not use_tasks and use_issues and not use_meetings:
+            values['alias_model'] = 'project.issue'
+        elif not use_tasks and not use_issues and use_meetings:
+            values['alias_model'] = 'calendar.event'
+        return {'value': values}
 
 
 class PragmaticReclamo(models.Model):
